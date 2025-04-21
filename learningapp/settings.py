d4bb1c4d0d5c9ffb4fc6ba1df6c068930f9f1211
croko22 <kvchambit@gmail.com>
2025-04-21 16:13:12 -0500

    update structure

--- learningapp/settings.py
+++ learningapp/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.core',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +88,8 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'users.User'
+ 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
