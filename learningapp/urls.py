099569439d27fd959fdb1461fd44998c68936b4d
croko22 <kvchambit@gmail.com>
2025-04-21 13:03:55 -0500

    update project setup

--- learningapp/urls.py
+++ learningapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for learningapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('core.urls')),
+     path('', include('learningapp.apps.core.urls')),
 ]
